860de481736a29f32fd2f2274e64d4fe749da7ca
Igor.U <i.usenko@dex-it.ru>
2023-12-14 15:09:52 +0200

    added components for players

---
+++ basketball-league/src/app/pages/players/player-new/player-new.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-player-new',
+  templateUrl: './player-new.component.html',
+  styleUrls: ['./player-new.component.scss']
+})
+export class PlayerNewComponent {
+
+}
--- basketball-league/src/app/pages/players/players.module.ts
+++ basketball-league/src/app/pages/players/players.module.ts
@@ -3,12 +3,20 @@ import { CommonModule } from '@angular/common';
 import {PlayersComponent} from "./players.component";
 import {RouterModule, RouterOutlet} from "@angular/router";
 import {playersRoutes} from "./players-routing.module";
+import { PlayerCardComponent } from './player-card/player-card.component';
+import { PlayerInfoComponent } from './player-info/player-info.component';
+import { PlayerNewComponent } from './player-new/player-new.component';
+import { PlayersListComponent } from './players-list/players-list.component';
 
 
 
 @NgModule({
   declarations: [
-    PlayersComponent
+    PlayersComponent,
+    PlayerCardComponent,
+    PlayerInfoComponent,
+    PlayerNewComponent,
+    PlayersListComponent
   ],
   imports: [
     CommonModule,
